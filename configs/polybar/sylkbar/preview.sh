4bf8cd33cfae69aa1f2b0986a25616efb30674aa
Sylk0s <sylkos49@gmail.com>
2021-11-27 15:52:03 -0500

    Changed bars

--- configs/polybar/sylkbar/preview.sh
+++ configs/polybar/sylkbar/preview.sh
@@ -10,10 +10,12 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the preview bar
 polybar -q top -c "$DIR"/preview.ini &
-polybar -q time -c "$DIR"/preview.ini &
-polybar -q keyboard -c "$DIR"/preview.ini &
-polybar -q audio -c "$DIR"/preview.ini &
-polybar -q memory -c "$DIR"/preview.ini &
-polybar -q file -c "$DIR"/preview.ini &
-polybar -q net -c "$DIR"/preview.ini &
-polybar -q cpu -c "$DIR"/preview.ini &
+#polybar -q time -c "$DIR"/preview.ini &
+#polybar -q keyboard -c "$DIR"/preview.ini &
+#polybar -q audio -c "$DIR"/preview.ini &
+#polybar -q memory -c "$DIR"/preview.ini &
+#polybar -q file -c "$DIR"/preview.ini &
+#polybar -q net -c "$DIR"/preview.ini &
+#polybar -q cpu -c "$DIR"/preview.ini &
+polybar -q left -c "$DIR"/preview.ini &
+polybar -q right -c "$DIR"/preview.ini &
