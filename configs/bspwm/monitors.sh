c7ca63aaff2426d50b62d1402ac30d62a197714c
sylkos <sylkos49@gmail.com>
2022-07-24 08:41:57 -0400

    buggy laptop autorandr config

---
+++ configs/bspwm/monitors.sh
@@ -0,0 +1,15 @@
+bspc subscribe monitor | while read -r line; do
+  case $line in
+      monitor_add*|monitor_geometry*)
+        if [ "$(bspc query -M | wc -l)" -eq "3" ]; then
+          bspc monitor DP-3-1 -d 1 2 3
+          bspc monitor DP-3-2 -d 4 5 6 7
+          bspc monitor eDP-1 -d 8 9 10 11
+        else
+          bspc monitor eDP-1 -d 1 2 3 4 5 6 7 8 9 10 11
+        fi
+        ;;
+      *)
+      ;;
+  esac
+done &
--- configs/sxhkd/scripts/floating_terminal.sh
+++ configs/sxhkd/scripts/floating_terminal.sh
@@ -2,4 +2,4 @@
 windowwidth=800
 windowheight=600
 
-bspc rule -a \* -o state=floating rectangle="${windowwidth}x${windowheight}+0+0" center=true && terminator
+bspc rule -a \* -o state=floating rectangle="${windowwidth}x${windowheight}+0+0" center=true && env LIBGL_ALWAYS_SOFTWARE=1 kitty
