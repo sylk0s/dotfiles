a10327d7a83d0dda6fc1374484c4f387190dbeb1
sylkos <sylkos49@gmail.com>
2022-12-03 15:41:30 -0500

    started moving scripts around

---
+++ scripts/base_packages.sh
@@ -0,0 +1,81 @@
+# htop man-db man-pages neofetch texinfo
+# sudo yay-git
+# zathura zathura-pdfpoppler
+#? wpa_supplicant-openssl1
+# xclip vscodium-bin
+
+#? volctl xcompmgr tumbler
+# ttf-isosevka-nerd
+# ttf-ms-win11-auto
+# tmux docker
+# thunar
+# spotify
+# spicetify-cli
+# sof-firmware
+# discord
+
+#? slack steam
+# rustup
+# rofi
+# ripgrep
+# rar
+# ranger
+# python-pip python
+# pulseaudio
+# prismlauncher-bin
+# pokemon-colorscripts-git
+#? pkgconf
+# picom
+# pavucontrol
+# papirus-icon-theme
+# p7zip
+# openvpn
+# openssh? ssh?
+# obsidian
+# obs
+# npm nodejs
+# nitrogen
+# networkmanager-openvpn
+# nerd-fonts-jetbrains-mono
+# mesa-utils?
+# maim?
+# make
+# lolcat
+# linux-headers
+# lightdm
+# lightdm-webkit-aether
+# lightdm-webkit2-greeter?
+# kubectl
+# julia
+# java shit
+# inxi?
+# intellij?
+# characters???
+# firefox
+# github-cli
+# gimp
+# gcc
+# ffmpegthumbnailer
+# feh fd? exa
+# dunst
+# downgrade
+# dkms
+# cmake
+# clion?
+# cava
+# catppuccin-gtk-theme
+# btop
+# blueberry?
+# bison?
+# betterdiscordctl-git
+# betterlockscreen-git
+# bash-pipes
+# arduino
+# fzf?
+# base-devel
+
+pacman -S xorg
+pacman -S sxhkd bspwm kitty
+pacman -S sudo
+# yay!
+# pacman -S lightdm lightdm-webkit2-greeter lightdm-webkit-theme-aether
---
+++ scripts/user.sh
@@ -0,0 +1,4 @@
+groupadd sudo
+useradd -m -G sudo sylkos
+echo "Passwd for user"
+passwd sylkos
