e6e6d9b638741a1c591bda44db1475d863822671
Sylkos <sylkos49@gmail.com>
2023-09-12 23:20:34 -0400

    finally fixed firefox

--- modules/desktop/apps/firefox.nix
+++ modules/desktop/apps/firefox.nix
@@ -204,7 +204,8 @@ in {
 #          Version=2
 #        '';
 #
-        "${cfgPath}/${cfg.profileName}.default/user.js" =
+				# hardcoded for laptop profile for now
+        "${cfgPath}/ahpu6nkm.default/user.js" =
           mkIf (cfg.settings != {} || cfg.extraConfig != "") {
             text = ''
               ${concatStrings (mapAttrsToList (name: value: ''
