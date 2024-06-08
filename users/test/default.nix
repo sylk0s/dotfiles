1d946c52c045516945e8dcfe42808668aa31231a
Sylkos <sylkos49@gmail.com>
2024-06-08 16:46:33 -0400

    help

--- users/test/default.nix
+++ users/test/default.nix
@@ -1,25 +1,25 @@
 {config, ...}: {
   modules = {
-    desktop = {
-      hyprland = {
-        enable = true;
-      };
-      social = {
-        discord.enable = true;
-      };
-      apps = {
-        firefox = {
-          enable = true;
-        };
-      };
-      services = {
-        ags = {
-          enable = true;
-        };
-      };
-    };
-    dev = {
-      julia.enable = true;
-    };
+    # desktop = {
+    # hyprland = {
+    #   enable = true;
+    # };
+    # social = {
+    #   discord.enable = true;
+    # };
+    # apps = {
+    #   firefox = {
+    #     enable = true;
+    #   };
+    # };
+    # services = {
+    #   ags = {
+    #     enable = true;
+    #   };
+    # };
+    # };
+    # dev = {
+    #   julia.enable = true;
+    # };
   };
 }
