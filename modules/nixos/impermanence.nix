484de70a03b37b1331dd6aa37173ae2fd42ab8a6
Sylkos <sylkos49@gmail.com>
2024-06-11 15:44:46 -0400

    script borkn

--- modules/nixos/impermanence.nix
+++ modules/nixos/impermanence.nix
@@ -22,28 +22,33 @@ in {
       mkdir /btrfs_tmp
       mount -t btrfs /dev/root_vg/root_v /btrfs_tmp
 
-      if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_root/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-      fi
+      btrfs subvolume delete /btrfs_tmp/root
+      btrfs subvolume snapshot /btrfs_tmp/root-blank /btrfs_tmp/root
 
-      delete_subvolute_rec() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-          delete_subvolume_rec "/btrfs_tmp/$i"
-        done
-        btrfs subvolume delete "$1"
-      }
-
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_rec "$i"
-      done
-
-      btrfs subvolume snapshot /mnt/root-blank /mnt/root
       umount /btrfs_tmp
       rmdir /btrfs_tmp
     '';
+
+    # btrfs subvolume delete /btrfs_tmp/home
+    #   btrfs subvolume snapshot /btrfs_tmp/root-blank /btrfs_tmp/home
+
+    # if [[ -e /btrfs_tmp/root ]]; then
+    #   mkdir -p /btrfs_root/old_roots
+    #   timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%d_%H:%M:%S")
+    #   mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    # fi
+
+    # delete_subvolute_rec() {
+    #   IFS=$'\n'
+    #   for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+    #     delete_subvolume_rec "/btrfs_tmp/$i"
+    #   done
+    #   btrfs subvolume delete "$1"
+    # }
+
+    # for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+    #   delete_subvolume_rec "$i"
+    # done
 
     environment.persistence."/persist" = {
       enable = true;
