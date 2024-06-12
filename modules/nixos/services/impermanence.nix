9ffe407e88f21021131da39c4fa30b6e99847295
Sylkos <sylkos49@gmail.com>
2024-06-12 11:22:44 -0400

    tried to fix the config... again

--- modules/nixos/services/impermanence.nix
+++ modules/nixos/services/impermanence.nix
@@ -20,68 +20,58 @@ in {
   config = mkIf cfg.enable {
     programs.fuse.userAllowOther = true;
 
-    boot.initrd = {
-      enable = true;
-      systemd.services.restore-root = {
-        description = "Wipes btrfs root";
-        wantedBy = ["initrd.target"];
-        requires = [
-          "dev-vda3"
-        ];
-        after = [
-          "dev-vda3"
-          "systemd-cryptsetup@${config.networking.hostName}.service"
-        ];
-        before = ["sysroot.mount"];
-        unitConfig.DefaultDependencies = "no";
-        serviceConfig.Type = "oneshot";
-        script = concatLines ([
-            ''
-              mkdir /btrfs_tmp
-              mount -t btrfs /dev/root_vg/root_v /btrfs_tmp
+    boot.initrd.postDeviceCommands = mkAfter (concatLines ([
+        ''
+          mkdir /btrfs_tmp
+          mount -t btrfs /dev/root_vg/root_v /btrfs_tmp
 
-              users=$(ls -1 /btrfs/home)
+          users=$(ls -1 /btrfs/home)
 
-              echo "deleting root recursively" &&
-              btrfs subvolume list -o /btrfs_tmp/root |
-              cut -f9 -d ' ' |
-              while read subvolume; do
-                echo "deleting /$subvolume subvolume..."
-                btrfs subvolume delete "/btrfs_tmp/$subvolume"
-              done &&
-              echo "deleting /root subvolume" &&
-              btrfs subvolume delete /btrfs_tmp/root &&
-              echo "restoring blank snapshot" &&
-              btrfs subvolume snapshot /btrfs_tmp/root-blank /btrfs_tmp/root
+          echo "deleting root recursively" &&
+          btrfs subvolume list -o /btrfs_tmp/root |
+          cut -f9 -d ' ' |
+          while read subvolume; do
+            echo "deleting /$subvolume subvolume..."
+            btrfs subvolume delete "/btrfs_tmp/$subvolume"
+          done &&
+          echo "deleting /root subvolume" &&
+          btrfs subvolume delete /btrfs_tmp/root &&
+          echo "restoring blank snapshot" &&
+          btrfs subvolume snapshot /btrfs_tmp/root-blank /btrfs_tmp/root
 
-              echo "deleting home recursively" &&
-              btrfs subvolume list -o /btrfs_tmp/home |
-              cut -f9 -d ' ' |
-              while read subvolume; do
-                echo "deleting /$subvolume subvolume..."
-                btrfs subvolume delete "/btrfs_tmp/$subvolume"
-              done &&
-              echo "deleting /home subvolume" &&
-              btrfs subvolume delete /btrfs_tmp/home &&
-              echo "restoring blank snapshot" &&
-              btrfs subvolume snapshot /btrfs_tmp/root-blank /btrfs_tmp/home
-            ''
-          ]
-          ++ (map (user:
-            optionalString user.createHome ''
-              mkdir -p /persist/${user.home}
-              chown -R ${user.name}:${user.group} /persist/${user.home}
-              chmod ${user.homeMode} /persist/${user.home}
-              echo "setup env for ${user.name}"
-            '') (attrValues config.users.users))
-          ++ [
-            ''
-              umount /btrfs_tmp
-              rmdir /btrfs_tmp
-            ''
-          ]);
-      };
-    };
+          echo "deleting home recursively" &&
+          btrfs subvolume list -o /btrfs_tmp/home |
+          cut -f9 -d ' ' |
+          while read subvolume; do
+            echo "deleting /$subvolume subvolume..."
+            btrfs subvolume delete "/btrfs_tmp/$subvolume"
+          done &&
+          echo "deleting /home subvolume" &&
+          btrfs subvolume delete /btrfs_tmp/home &&
+          echo "restoring blank snapshot" &&
+          btrfs subvolume snapshot /btrfs_tmp/root-blank /btrfs_tmp/home
+        ''
+      ]
+      ++ (map (user:
+        optionalString user.createHome ''
+          mkdir -p /btrfs_tmp/${user.home}
+          chown -R ${user.name}:${user.group} /btrfs_tmp/${user.home}
+          chmod ${user.homeMode} /btrfs_tmp/${user.home}
+
+          if [[ ! -e /btrfs_tmp/persist/${user.home} ]]; then
+            mkdir -p /btrfs_tmp/persist/${user.home}
+            chown -R ${user.name}:${user.group} /btrfs_tmp/persist/${user.home}
+            chmod ${user.homeMode} /btrfs_tmp/persist/${user.home}
+          fi
+
+          echo "setup env for ${user.name}"
+        '') (attrValues config.users.users))
+      ++ [
+        ''
+          umount /btrfs_tmp
+          rmdir /btrfs_tmp
+        ''
+      ]));
 
     environment.persistence."/persist" = {
       enable = true;
