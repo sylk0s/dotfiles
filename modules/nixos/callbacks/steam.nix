446d1d1f97ea97bb27915719febcb2fe4493f6c1
Sylkos <sylkos49@gmail.com>
2024-07-13 11:09:57 -0400

    steam is Not real

--- modules/nixos/callbacks/steam.nix
+++ modules/nixos/callbacks/steam.nix
@@ -6,7 +6,12 @@
 with lib;
 with lib.sylkos; {
   config = mkIf (anyUsers (user: user.modules.desktop.gaming.steam.enable) config.home-manager.users) {
-    programs.steam.enable = true;
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
 
     # better for steam proton games
     systemd.extraConfig = "DefaultLimitNOFILE=1048576";
