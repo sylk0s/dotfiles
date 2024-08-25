cd2fe7f75442f7420113b093440d84aafc83a391
Sylkos <sylkos49@gmail.com>
2024-08-25 14:26:54 -0400

    cantelope

--- tmp/configuration.nix
+++ tmp/configuration.nix
@@ -1,7 +1,19 @@
 {pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
+    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
+    ./disko-config.nix
   ];
+
+  fileSystems."/persist".neededForBoot = true;
+  fileSystems."/var/log".neededForBoot = true;
+
+  nixpkgs = {
+    overlays = [
+      (import ./grub_overlay.nix)
+    ];
+    hostPlatform.system = "x86_64-linux";
+  };
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
   boot = {
@@ -9,24 +21,25 @@
     supportedFilesystems = ["btrfs"];
     loader = {
       efi.canTouchEfiVariables = true;
-      efi.efiSysMountPoint = "/boot";
+      efi.efiSysMountPoint = "/boot/efi";
       grub = {
         enable = true;
         device = "nodev";
         efiSupport = true;
-        copyKernels = true;
+        #copyKernels = true;
+        enableCryptodisk = true;
         configurationLimit = 10;
       };
     };
 
-    initrd = {
-      luks.devices = {
-        "crypt" = {
-          device = "/dev/disk/by-uuid/UUID";
-          preLVM = true;
-        };
-      };
-    };
+    # initrd = {
+    #   luks.devices = {
+    #     "crypt" = {
+    #       device = "/dev/disk/by-uuid/UUID";
+    #       preLVM = true;
+    #     };
+    #   };
+    # };
   };
 
   i18n.defaultLocale = "en_US.UTF-8";
