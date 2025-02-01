dd922c12ee57baf99edc7c69aa9f2dd2ba330342
sylk0s <julia@sylkos.xyz>
2025-02-01 11:33:32 -0500

    added persist project dirs, backups for persist

--- modules/nixos/system/fs/ephemeral-btrfs.nix
+++ modules/nixos/system/fs/ephemeral-btrfs.nix
@@ -58,6 +58,12 @@ in {
               mv /btrfs_tmp/home "/btrfs_tmp/old_homes/$timestamp"
           fi
 
+          # backup persist
+          if [[ -e /btrfs_tmp/persist ]]; then
+              mkdir -p /btrfs_tmp/old_persist
+              cp /btrfs_tmp/persist "/btrfs_tmp/old_persist/$timestamp"
+          fi
+
           delete_subvolume_recursively() {
               IFS=$'\n'
               for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
@@ -76,10 +82,17 @@ in {
               delete_subvolume_recursively "$i"
           done
 
+          # deletes old saved persist
+          # this only saves a week as opposed to the others, which save a month
+          for i in $(find /btrfs_tmp/old_persist/ -maxdepth 1 -mtime +7); do
+              delete_subvolume_recursively "$i"
+          done
+
           # recreates subvolumes
           btrfs subvolume create /btrfs_tmp/root
           btrfs subvolume create /btrfs_tmp/home
 
+          # NOTE: This bug wasn't fixed, I made home needed for boot to fix this issue
           # creates user home directories
           # see https://github.com/NixOS/nixpkgs/issues/6481
           # will be fixed by https://github.com/NixOS/nixpkgs/pull/223932
